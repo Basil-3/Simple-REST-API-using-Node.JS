3e5e1c31e9a3bd0f3824400e90be68004d91a95d
Basil Singh <basil@iitk.ac.in>
2020-05-13 22:25:23 +0530

    Update products.js

--- api/routers/products.js
+++ api/routers/products.js
@@ -1,1 +1,31 @@
+const express = require('express');
+const router = express.Router();
+const multer = require('multer');
+const checkAuth = require('../middleware/check-auth');
+const ProductsController = require('../controllers/products');
 
+const storage = multer.diskStorage({
+    destination: function(req, file, cb){
+        cb(null, './uploads/');
+    },
+    filename: function(req, file, cb){
+        cb(null, new Date().toISOString() + file.originalname);
+    }
+});
+
+const fileFilter = (req, file, cb) => {
+    //rejecting files with inappropriate file mimetype
+    if(file.mimetype === 'image/jpeg' || file.mimetype === 'image/png'){
+        cb(null, true);
+    }else{
+        cb(null,false);
+    }
+}
+
+const upload = multer({
+    storage: storage, 
+    limits: {
+        fileSize: 1024*1024*6
+    },
+    fileFilter: fileFilter
+});
